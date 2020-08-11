3485052c9a2ae1807f46a60e52dc80cd49db2bef
Mike Stephane <33469024+mkanyar@users.noreply.github.com>
2020-08-10 22:53:11 -0400

    adding the tasks to an array

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [item, setItem] = useState("");
+  const [array, setArray] = useState([]);
+  function handleChange(event) {
+    const itemName = event.target.value;
+    setItem(itemName);
+  }
+  function handleClick() {
+    setArray((prevValue) => {
+      return [...prevValue, item];
+    });
+    setItem("");
+  }
   return (
     <div className="container">
       <div className="heading">
-        <h1>To-Do List</h1>
+        <h1>To-Do List </h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input type="text" onChange={handleChange} value={item} />
+        <button onClick={handleClick}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {array.map((task, i) => (
+            <li key={i}>{task}</li>
+          ))}
         </ul>
       </div>
     </div>
